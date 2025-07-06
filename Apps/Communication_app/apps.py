7f80522f879dbf2e0687b697c0a62955c04ced12
saitej-a <ankamsaiteja27@gmail.com>
2025-07-06 14:31:41 +0530

    Init Setup

---
+++ Apps/Communication_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CommunicationAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Communication_app'
---
+++ Apps/Fee_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FeeAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Fee_app'
