2c03d9d7cfbe18f60fe458699b9982647cf696bf
saitej-a <ankamsaiteja27@gmail.com>
2025-07-10 11:57:49 +0530

    added user register api's

--- Apps/CustomUsers_app/views.py
+++ Apps/CustomUsers_app/views.py
@@ -2,14 +2,15 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .serializers import CustomUserSerializer
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes,api_view
+from .permissions import IsAdmin,IsParent,IsStudent,IsTeacher,IsTeacherOrAdmin
 
 User=get_user_model()
 class UserViewset(viewsets.ModelViewSet):
     queryset=User.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsTeacherOrAdmin]
 
     
 @api_view(['GET'])
@@ -23,6 +24,3 @@ def register(request):
         serializer.save()
         return Response({"message": "Successfully Created"}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-@api_view(['POST'])
-def login(request):
-  pass
