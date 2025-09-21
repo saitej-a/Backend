ba3d85f9bfbb75e71dfcef29f2af8dc51346e6b8
KSHITIJ3810 <kshitijvats57@gmail.com>
2025-09-21 16:09:09 +0530

    added Announcement API and Feedback model

--- Apps/Communication_app/models.py
+++ Apps/Communication_app/models.py
@@ -23,23 +23,3 @@ class Announcement(models.Model):
     def __str__(self):
         return self.title
 
-#Feedback model
-from django.conf import settings
-
-class Feedback(models.Model):
-    ROLE_CHOICES = [
-        ('student', 'Student'),
-        ('parent', 'Parent'),
-        ('teacher', 'Teacher'),
-        ('admin', 'Admin'),
-    ]
-
-    title = models.CharField(max_length=100)
-    message = models.TextField()
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    resolved = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.title} - {self.role} - {self.created_at.strftime('%Y-%m-%d')}"
